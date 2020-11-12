8984fbe9ffaf192eb04952eaa2104016c30e3a87
Đỗ Thành Đạt <73975681+dat27102001@users.noreply.github.com>
2020-11-12 15:58:58 +0700

    F

---
+++ ifesleif.js
@@ -0,0 +1,18 @@
+let username= prompt("Who are u bro ?");
+if (username == 'Admin'){
+    alert('Welcome to the hell');
+}    else if (username == null){
+    alert ('Canceled');
+}    else {
+    alert("get away (._|_.) =))");
+}
+    var pass = prompt('Password?')
+    if (pass == 'youaremyluv'){
+        alert(pass= 'dame you bro');
+    } else if (pass == null){
+        alert('Come in');
+    }   else {
+        alert("Get away now");
+    }
+
+
